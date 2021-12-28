97ec3b733fd5b4260d3a331cc638bdd216c6765b
obaidjani <muhammad.obaid@dazaran.com>
2021-12-28 13:11:53 +0500

    comma and decimal allowed

--- resources/views/leads/create.blade.php
+++ resources/views/leads/create.blade.php
@@ -68,14 +68,14 @@ Zellaray Capital
                   </div>
                   <div class="form-group col-md-4">
                     <label class="mb-2 formlabel">Ask Price</label>
-                    <input type="number" class="form-control" id="ask_price" step=".01" name="ask_price" placeholder="Ask Price" required>
+                    <input type="text" class="form-control" id="ask_price" name="ask_price" placeholder="Ask Price" required>
                     @if($errors->has('ask_price'))
                     <div class="error">{{ $errors->first('ask_price') }}</div>
                     @endif
                   </div>
                   <div class="form-group col-md-4">
                     <label class="mb-2 formlabel">Price Per Door</label>
-                    <input type="number" class="form-control" id="price_per_door" name="price_per_door" placeholder="Price per door" required>
+                    <input type="text" class="form-control" id="price_per_door" name="price_per_door" placeholder="Price per door" required>
                     @if($errors->has('price_per_door'))
                     <div class="error">{{ $errors->first('price_per_door') }}</div>
                     @endif
@@ -96,7 +96,7 @@ Zellaray Capital
                   </div>
                   <div class="form-group col-md-4">
                     <label class="mb-2 formlabel">Cap Rate</label>
-                    <input type="number" class="form-control" id="cap_rate" name="cap_rate" step=".01" placeholder="Cap Rate">
+                    <input type="text" class="form-control" id="cap_rate" name="cap_rate" step=".01" placeholder="Cap Rate">
                     @if($errors->has('cap_rate'))
                     <div class="error">{{ $errors->first('cap_rate') }}</div>
                     @endif
@@ -268,5 +268,18 @@ Zellaray Capital
 
     });
   }
+</script>
+
+<script>
+  $("#ask_price,#price_per_door,#cap_rate").on("keypress keyup blur", function(event) {
+    $(this).val($(this).val().replace(/[^0-9\.|\,]/g, ''));
+    if (event.which == 44) {
+      return true;
+    }
+    if ((event.which != 46 || $(this).val().indexOf('.') != -1) && (event.which < 48 || event.which > 57)) {
+
+      event.preventDefault();
+    }
+  });
 </script>
 @endsection
